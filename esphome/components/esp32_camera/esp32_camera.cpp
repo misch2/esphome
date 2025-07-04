0f21f0c4041fc61f83a348f73ef79f83985845aa
Michal Schwarz <michal.schwarz@gmail.com>
2025-07-04 21:31:06 +0200

    wip

--- esphome/components/esp32_camera/esp32_camera.cpp
+++ esphome/components/esp32_camera/esp32_camera.cpp
@@ -132,6 +132,7 @@ void ESP32Camera::dump_config() {
   ESP_LOGCONFIG(TAG, "  Horizontal Mirror: %s", ONOFF(st.hmirror));
   ESP_LOGCONFIG(TAG, "  Special Effect: %u", st.special_effect);
   ESP_LOGCONFIG(TAG, "  White Balance Mode: %u", st.wb_mode);
+  ESP_LOGCONFIG(TAG, "  Night mode: %u", ONOFF(s->get_reg(s, 0x10f, 0xff) == 0x4b));
   // ESP_LOGCONFIG(TAG, "  Auto White Balance: %u", st.awb);
   // ESP_LOGCONFIG(TAG, "  Auto White Balance Gain: %u", st.awb_gain);
   ESP_LOGCONFIG(TAG, "  Auto Exposure Control: %u", st.aec);
@@ -318,6 +319,8 @@ void ESP32Camera::set_agc_value(uint8_t agc_value) { this->agc_value_ = agc_valu
 void ESP32Camera::set_agc_gain_ceiling(ESP32AgcGainCeiling gain_ceiling) { this->agc_gain_ceiling_ = gain_ceiling; }
 /* set white balance */
 void ESP32Camera::set_wb_mode(ESP32WhiteBalanceMode mode) { this->wb_mode_ = mode; }
+/* set night mode */
+void ESP32Camera::set_night_mode(uint8_t mode) { this->night_mode_ = mode; }
 /* set test mode */
 void ESP32Camera::set_test_pattern(bool test_pattern) { this->test_pattern_ = test_pattern; }
 /* set fps */
@@ -373,6 +376,16 @@ void ESP32Camera::update_camera_parameters() {
   s->set_gainceiling(s, (gainceiling_t) this->agc_gain_ceiling_);
   /* update white balance mode */
   s->set_wb_mode(s, (int) this->wb_mode_);  // 0 to 4
+  /* update night mode */
+  //  see this for emulation of WcFeature from Tasmota:
+  //  https://github.com/arendst/Tasmota/blob/67a62ef3c8297409d0bc58b511e4958ebae577ed/tasmota/tasmota_xdrv_driver/xdrv_81_esp32_webcam.ino#L219
+  if (this->night_mode_ == 0) {
+      s->set_reg(s, 0x103, 0xff, 0x0a);  // COM1: Reset dummy frames
+      s->set_reg(s, 0x10f, 0xff, 0x43);  // RSVD: disable nightmode
+  } else {  // mode 1 or 2
+      s->set_reg(s, 0x10f, 0xff, 0x4b);  // RSVD: enable nightmode
+      s->set_reg(s, 0x103, 0xff, 0xcf);  // COM1: Set dummy frames to 7
+  }
   /* update test pattern */
   s->set_colorbar(s, this->test_pattern_);
 }
